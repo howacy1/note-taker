f3e3af690b21d4a5cee1f9901bb60d5903632a33
Cy Howard <cyhoward47@gmail.com>
2022-08-11 22:31:28 -0500

    comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,21 @@
-
+//dependencies
 const path = require('path');
 const fs = require('fs')
 
-
+//npm package for unique ids
 var uniqid = require('uniqid');
 
 
 
 module.exports = (app) => {
 
- 
+  // GET /api/notes reads to db.json file and reutn all saved notes as JSON
   app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
   });
 
-  
+  // POST /api/notes receives a new note to save on the request body, 
+  // add it to the db.json file, and then return the new note to the client
   app.post('/api/notes', (req, res) => {
     let db = fs.readFileSync('db/db.json');
     db = JSON.parse(db);
@@ -34,7 +35,7 @@ module.exports = (app) => {
   });
 
 
-  
+  // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete
   app.delete('/api/notes/:id', (req, res) => {
     
     let db = JSON.parse(fs.readFileSync('db/db.json'))
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,15 @@
+//dependencies
 const path = require('path');
 
 
 module.exports = (app) => {
 
-  
+  // GET /notes returns notes.html
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  
+  // GET /notes returns index.html
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   })
