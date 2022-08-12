0c85426f7c7bbf9dd03c91e8a70889b77d76f0da
Cy Howard <cyhoward47@gmail.com>
2022-08-11 22:15:50 -0500

    fixing routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,25 +1,48 @@
+
 const path = require('path');
 const fs = require('fs')
-const router = require('express').Router();
+
 
 var uniqid = require('uniqid');
 
-router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-});
 
-router.post('/api/notes', (req, res) => {
+
+module.exports = (app) => {
+
+ 
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+
+  
+  app.post('/api/notes', (req, res) => {
     let db = fs.readFileSync('db/db.json');
     db = JSON.parse(db);
     res.json(db);
+    
     let userNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uniqid()
+      title: req.body.title,
+      text: req.body.text,
+      
+      id: uniqid(),
     };
+    
     db.push(userNote);
     fs.writeFileSync('db/db.json', JSON.stringify(db));
     res.json(db);
-});
 
-module.exports = router;
+  });
+
+
+  
+  app.delete('/api/notes/:id', (req, res) => {
+    
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+    
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+    
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+    
+  })
+};
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
+
+const PORT = process.env.PORT || 3001;
+
+
+
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`Server available at localhost${PORT}`);
 });
