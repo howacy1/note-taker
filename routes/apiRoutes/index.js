5b877066d7123df09c5864ffd10f1d0b925a9753
Cy Howard <cyhoward47@gmail.com>
2022-08-11 21:52:09 -0500

    finished route js pages

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs')
+const router = require('express').Router();
+
+var uniqid = require('uniqid');
+
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+router.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    let userNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqid()
+    };
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+});
+
